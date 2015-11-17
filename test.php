2b88d0df6d681933396762f102ba137e9b40489d
Paul <sirpaul>
2015-11-16 16:59:12 -0800

    This is my second change to test.php
    Always speak in present tense

--- test.php
+++ test.php
@@ -2,10 +2,10 @@
 <head>
 <body>
 <h1>Imagine this is a cool web page!</h1>
-<p>Version 2</p>
+<p>Version 3</p>
+<p>Different clever content goes here</p>
 <p>Clever content goes here</p>
-<p>Clever content goes here</p>
-<p>Clever content goes here</p>
+<p>This is also different clever content.</p>
 </body>
 </head>
 </html>
