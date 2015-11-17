508662ebbd1b72aa53bf6d7aff78e556b0879956
Paul <sirpaul>
2015-11-16 16:51:56 -0800

    Initial Test Page Made

---
+++ test.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<body>
+<h1>Imagine this is a cool web page!</h1>
+<p>Version 2</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+<p>Clever content goes here</p>
+</body>
+</head>
+</html>
